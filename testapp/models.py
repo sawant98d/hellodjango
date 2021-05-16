b92a4012d0e0a495983ad182903dab98954b3069
sawand98d <sawant65d@gmail.com>
2021-05-16 18:55:07 +0530

    Database Handling- Using admin app- registerd model class to admin.py and accessed emp database in admin app

--- testapp/models.py
+++ testapp/models.py
@@ -6,3 +6,6 @@ class Employee(models.Model):
     ename = models.CharField(max_length=30)
     esal = models.FloatField()
     eadd = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return str(self.ename)
