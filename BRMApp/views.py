69e4cedd2ce52049cda267cc1e275cc9ec735afd
sawand98d <sawant65d@gmail.com>
2021-05-22 21:12:25 +0530

    Example Project | CRUD- Add new Book

--- BRMApp/views.py
+++ BRMApp/views.py
@@ -1,3 +1,24 @@
+from django.forms import models
+from django.http.response import HttpResponse
+from BRMApp.forms import NewBookForm
 from django.shortcuts import render
+from BRMApp.forms import NewBookForm
+
 
 # Create your views here.
+def newBook(request):
+    form = NewBookForm()
+    res = render(request, 'BRMApp/new_book.htm',{'form':form})
+    return res
+
+def add(request):
+    if request.method == 'POST':
+        form = NewBookForm(request.POST)
+        book = models.Book()
+        book.title = form.data['title']
+        book.price = form.data['price']
+        book.auther = form.data['auther']
+        book.publisher = form.data['publisher']
+        book.save()
+    s = "Record stored <br> <a href='/BRMApp/view-books'>View all books</a>"
+    return HttpResponse(s)
