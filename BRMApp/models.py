527dbd927e5c96e2ec5a594e39a3d1a5969f338f
sawand98d <sawant65d@gmail.com>
2021-05-23 16:28:28 +0530

    Login Logout- Creating Models

--- BRMApp/models.py
+++ BRMApp/models.py
@@ -6,3 +6,7 @@ class Book(models.Model):
     price = models.FloatField()
     author = models.CharField(max_length=100)
     publisher = models.CharField(max_length=100)
+
+class BRMUser(models.Model):
+    user = models.OneToOneField(User, on_delete="models.CASCADE")
+    nickname = models.CharField(max_length=20, null=False)
