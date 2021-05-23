7874cf0432f1d83703a3403b9bb4214591fd17fe
sawand98d <sawant65d@gmail.com>
2021-05-23 07:07:16 +0530

    Example Project | View Books Records

--- BRMApp/views.py
+++ BRMApp/views.py
@@ -3,9 +3,17 @@ from django.http.response import HttpResponse
 from BRMApp.forms import NewBookForm
 from django.shortcuts import render
 from BRMApp.forms import NewBookForm
-
+from django.http import HttpResponse
+from BRMApp import models
 
 # Create your views here.
+
+def viewBooks(request):
+    books = models.Book.objects.all()
+    res = render(request, 'BRMApp/view_book.htm', {'books':books})
+    return res
+
+
 def newBook(request):
     form = NewBookForm()
     res = render(request, 'BRMApp/new_book.htm',{'form':form})
